1b33ff939129d0e4421ee4a762d8ca190bbb01d3
Fred Yang <fredyang@semanticsworks.com>
2011-04-28 07:39:51 -0400

    remove handler dependencies in config file

--- sample/js/matrix/matrix.config.js
+++ sample/js/matrix/matrix.config.js
@@ -1,8 +1,7 @@
 matrix.depend( {
 	"ui.theme.module": "redmond.uitheme",
 	"ui.css.module": "ui.core.uicss, ui.theme.module",
-	"dialog.module": "ui.css.module, dialog.uijs",
-	"ui.module" : "matrix.uijs.js, matrix.uicss.js, matrix.uitheme.js"
+	"dialog.module": "ui.css.module, dialog.uijs"
 } );
 
 matrix.baseUrl = "js/";
