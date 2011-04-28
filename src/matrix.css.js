d2f7066cc1a82e0585efe4605f5ba5b2714bd373
Fred Yang <fredyang@semanticsworks.com>
2011-04-28 07:51:06 -0400

    add some remark and change the selector to select the css link

--- src/matrix.css.js
+++ src/matrix.css.js
@@ -5,16 +5,18 @@
 
 	matrix.addHandler( "css", {
 		load: matrix.buildLoad(
+			//checkPreload
 			function ( url ) {
 				return !!($( "link" ).filter(
 					function () {
-						return this.href === url && !this.matrix;
+						return this.href === url &&  $(this).attr('matrix');
 					} ).length);
 			},
+			//buildSourceEvaluator
 			function ( resource, url, sourceCode ) {
 				var promise = matrix.promises( resource );
 				return function () {
-					matrix.log( "add css @ <link href='" + url + "' " + "rel='stylesheet' type='text/css' />" );
+					matrix.log( "\trunning css " + url );
 					$( "<link href='" + url + "' " + "rel='stylesheet' type='text/css' matrix='true' />" ).appendTo( "head" );
 					setTimeout( function () {
 						promise.defer.resolve();
@@ -26,7 +28,7 @@
 			var url = matrix.url( resource );
 			$( "link" ).filter(
 				function () {
-					return this.href === url;
+					return this.href === url && $(this).attr('matrix');
 				} ).remove();
 		}
 	} );
