2ed3a761132062724e9d32b71e9d01b76a8e119d
Fred Yang <fredyang@semanticsworks.com>
2011-04-30 00:49:50 -0400

    change test config file

--- sample/js/matrix/matrix.config.js
+++ sample/js/matrix/matrix.config.js
@@ -6,17 +6,10 @@ matrix.depend( {
 	"uicss.module": "core.uicss, uitheme.module"
 } );
 
-$.each( "accordion,autocomplete,button,datepicker,dialog,progressbar,resizable,selectable,slider,tabs".split( "," ),
-	function (index, value) {
-		var resourceKey = value + ".module";
-		var depends = value + ".uijs, " + value +  ".uicss, uicss.module";
-		matrix.depend(resourceKey, depends);
-	} );
-
-
-$.each( "draggable,droppable,mouse,position,sortable".split( "," ),
+$.each( ("accordion,autocomplete,button,datepicker,dialog,progressbar,resizable,selectable,slider,tabs," + "draggable,droppable,mouse,position,sortable").split( "," ),
 	function (index, value) {
 		var resourceKey = value + ".module";
 		var depends = value + ".uijs, uicss.module";
 		matrix.depend(resourceKey, depends);
 	} );
+
