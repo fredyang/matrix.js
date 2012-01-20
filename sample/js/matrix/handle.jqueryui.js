e905ce5046b2caa520d82d3de55f002b87d5a0c5
Fred Yang <fredyang@semanticsworks.com>
2012-01-20 12:03:19 -0500

    add remark

--- sample/js/matrix/handle.jqueryui.js
+++ sample/js/matrix/handle.jqueryui.js
@@ -1,16 +1,28 @@
 //add jqueryui_css handler
 (function( $, matrix ) {
 
+
 	matrix.addHandler( "jqueryui", "module", {
-		//a moduleName.jqueryui depends on moduleName.jquery_js, core.jqueryui_css, jqueryui_theme.module
-		depend: function ( resourceKey ) {
+		// a moduleName.jqueryui depends on
+		// moduleName.jquery_js,
+		// core.jqueryui_css,
+		// jqueryui_theme.module
+		//
+		// for example
+		// dialog.jqueryui depends on
+		//
+		// dialog.jquery_js (base js extend url, parse(depends))
+		// core.jqueryui_css (base css extend url)
+		// jqueryui_theme.module (which depends on one of the themes such as
+		// redmond.jqueryui_theme, which is base css extend url)
+		depend: function( resourceKey ) {
 			var moduleName = matrix.resourceName( resourceKey );
 			return moduleName + ".jqueryui_js,core.jqueryui_css, jqueryui_theme.module";
 		}
 	} );
 
 	matrix.addHandler( "jqueryui_css", "css", {
-		url: function ( resourceKey ) {
+		url: function( resourceKey ) {
 			return matrix.resourceBaseUrl + "jquery.ui/css/base/jquery.ui." + matrix.resourceName( resourceKey ) + ".css";
 		}
 	} );
@@ -21,28 +33,30 @@
 	//extend js handler
 	matrix.addHandler( "jqueryui_js", "js", {
 
-		url: function ( resourceKey ) {
+		url: function( resourceKey ) {
 			return  matrix.resourceBaseUrl + "jquery.ui/jquery.ui." + matrix.resourceName( resourceKey ) + ".min.js";
 		},
 
-		parse: function ( resourceKey, sourceCode ) {
+		parse: function( resourceKey, sourceCode ) {
 			var dependencies = [],
 				module,
 				dependText = rdependencies.exec( sourceCode );
 
-			if ( dependText = dependText && dependText[1] ) {
+			if (dependText = dependText && dependText[1]) {
 				//dependText is something like
 				/*
 				 *	jquery.ui.tabs.css
 				 *	jquery.ui.core.js
 				 *	jquery.ui.widget.js
 				 */
-				while ( module = ruiModule.exec( dependText ) ) {
+				while (module = ruiModule.exec( dependText )) {
 					//find jquery.ui.xxx.css and convert it to xxx.jqueryui_css
 					//find jquery.ui.xxx.js and convert it to xxx.jqueryui_js
 
-					// "core" + ".ui" + "js" == "core.jqueryui_js"
-					// "core" + ".ui" + "css" == "core.jqueryui_css"
+					//module[1] is "xxx"
+					//module[2] is "js" or "css"
+					// "xxx" + ".ui" + "js" == "xxx.jqueryui_js"
+					// "xxx" + ".ui" + "css" == "xxx.jqueryui_css"
 					dependencies.push( module[1] + ".jqueryui_" + module[2] );
 				}
 				return dependencies.length ? dependencies.toString() : null;
@@ -53,7 +67,7 @@
 	} );
 
 	matrix.addHandler( "jqueryui_theme", "css", {
-		url: function ( resourceKey ) {
+		url: function( resourceKey ) {
 			return matrix.resourceBaseUrl + "jquery.ui/css/" + matrix.resourceName( resourceKey ) + "/jquery.ui.theme.css";
 		}
 	} );
